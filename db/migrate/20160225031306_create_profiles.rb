b810183d4eb88750d422ece28219e9b962863995
Rudolf Malle <malle3rudolf@gmail.com>
2016-02-25 03:43:15 +0000

    Updated the database

--- db/migrate/20160225031306_create_profiles.rb
+++ db/migrate/20160225031306_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
-      t.interger :user_id
+      t.integer :user_id
       t.string :first_name
       t.string :last_name
       t.string :job_title
