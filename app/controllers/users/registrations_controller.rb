45b030791f9d7d5f8afa784f025701d451c6b8cd
Rudolf Malle <malle3rudolf@gmail.com>
2016-02-25 03:06:44 +0000

    Updated the Gemfile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,11 +14,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
         end
     end 
     
-    private
-        def select_plan
-            unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2') 
-                flash[:notice] = "please select a membership plan to sign up."
-                redirect_to root_url
+private
+    def select_plan
+        unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2') 
+            flash[:notice] = "please select a membership plan to sign up."
+            redirect_to root_url
         end 
     end 
 end 
